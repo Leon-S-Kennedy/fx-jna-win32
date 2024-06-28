a718850cbf63b232e53777641180520cdb37ed77
Leon-S-Kennedy <937854328@qq.com>
2024-06-29 01:27:03 +0800

    2024年6月29日01点26分

---
+++ src/main/java/com/libowen/Main.java
@@ -0,0 +1,8 @@
+package com.libowen;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
---
+++ src/main/java/com/libowen/MainApp.java
@@ -0,0 +1,22 @@
+package com.libowen;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
